8dbf06b9a462d43324e4017d1fd84141653daaa5
Isaac Hinman <isaac@isaachinman.com>
2018-01-14 19:30:03 +0100

    Update models, enable strict mode

--- src/models/comment.js
+++ src/models/comment.js
@@ -2,17 +2,41 @@ import mongoose, { Schema } from 'mongoose'
 
 const CommentSchema = new Schema({
   author: {
-    firstName: { type: String, min: 1, max: 50 },
-    lastName: { type: String, min: 1, max: 50 },
+    firstName: {
+      type: String,
+      min: 1,
+      max: 50,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      min: 1,
+      max: 50,
+      required: true,
+    },
     avatarURL: {
       type: String,
+      min: 1,
+      max: 500,
       default: 'http://www.europe-together.eu/wp-content/themes/sd/images/user-placeholder.svg',
     },
   },
   date: { type: Date, default: Date.now },
   isLiked: { type: Boolean, default: false },
-  text: { type: String, min: 1, max: 500 },
+  text: {
+    type: String,
+    min: 1,
+    max: 50,
+    required: true,
+  },
+}, {
+
+  // Throw errors for unspecified fields instead of quietly ignoring them
+  strict: 'throw',
+
 })
+
+CommentSchema.set('toJSON', { getters: true, virtuals: false })
 
 const CommentModel = mongoose.model('Comment', CommentSchema)
 
--- src/models/todo.js
+++ src/models/todo.js
@@ -1,10 +1,28 @@
 import mongoose, { Schema } from 'mongoose'
 
 const TodoSchema = new Schema({
-  done: { type: Boolean, default: false },
-  deadline: { type: Date, default: Date.now },
-  description: { type: String, min: 1, max: 500 },
+  done: {
+    type: Boolean,
+    default: false,
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    min: 1,
+    max: 500,
+    required: true,
+  },
+}, {
+
+  // Throw errors for unspecified fields instead of quietly ignoring them
+  strict: 'throw',
+
 })
+
+TodoSchema.set('toJSON', { getters: true, virtuals: false })
 
 const TodoModel = mongoose.model('Todo', TodoSchema)
 
